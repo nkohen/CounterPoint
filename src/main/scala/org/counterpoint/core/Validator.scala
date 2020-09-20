b7149a1c690c2e445c370220bc0d3a1f9f874fec
nkohen <nadavk25@gmail.com>
2020-09-20 13:23:49 -0500

    Implemented global validation and integrated global, measure, and line validation

--- src/main/scala/org/counterpoint/core/Validator.scala
+++ src/main/scala/org/counterpoint/core/Validator.scala
@@ -27,6 +27,51 @@ object Validator {
   // TODO: Warn about heuristic violations
   // TODO: Read from MusicXML file
   def validate(measures: Vector[Measure]): Evaluation = {
-    ???
+    val allNotes = measures.foldLeft(Vector.empty[Note]) {
+      case (notes, measure) =>
+        notes ++ measure.notes
+    }
+    val halfBeneathTonic = Note(measures.last.givenNote.id - 1)
+    val (_, globalRules) =
+      allNotes.tail.foldLeft[(Note, Evaluation)]((allNotes.head, Pass)) {
+        case ((note1, eval), note2) =>
+          val newEval = for {
+            _ <- eval
+            _ <- rule(note1 != note2,
+                      s"No notes ($note1) may be repeated consecutively")
+            _ <- rule(
+              note1 > note2 || note2 != halfBeneathTonic,
+              s"May not leap to half step beneath the tonic: ($note1, $note2)")
+          } yield Pass
+          (note2, newEval)
+      }
+
+    val (_, lastLine, linesWithoutLast) = allNotes.tail.foldLeft(
+      (allNotes.head, Vector(allNotes.head), Vector.empty[Line])) {
+      case ((note1, currentLine, lines), note2) =>
+        if (currentLine.length < 2) {
+          (note2, currentLine :+ note2, lines)
+        } else {
+          val ascending = currentLine(0) < currentLine(1)
+          if ((ascending && note1 < note2) || (!ascending && note1 > note2)) {
+            (note2, currentLine :+ note2, lines)
+          } else {
+            (note2, Vector(note1, note2), lines :+ Line(currentLine))
+          }
+        }
+    }
+    val lines = linesWithoutLast :+ Line(lastLine)
+
+    for {
+      _ <- globalRules
+      _ <- measures.foldLeft[Evaluation](Pass) {
+        case (eval, measure) =>
+          eval.flatMap(_ => measure.validateMeasureRules)
+      }
+      _ <- lines.foldLeft[Evaluation](Pass) {
+        case (eval, line) =>
+          eval.flatMap(_ => line.validateLineRules)
+      }
+    } yield Pass
   }
 }
